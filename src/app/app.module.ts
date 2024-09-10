9f5ff1acb113cbd0dab3768ab12b38b3b8775271
elie <el415698@dal.ca>
2024-09-10 12:16:59 +0200

    user module and login-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,21 @@ import { AppComponent } from './app.component';
 import {MatButtonModule} from '@angular/material/button';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { SharedModule } from './shared/shared.module';
+import { HomepageComponent } from './homepage/homepage.component';
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from "@angular/fire/compat";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [
     provideClientHydration(),
---
+++ src/app/user/user.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+
+@NgModule({
+  declarations: [
+    LoginPageComponent
+  ],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    SharedModule
+
+  ]
+})
+export class UserModule { }
