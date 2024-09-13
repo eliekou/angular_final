742d68c4370dea5f4c869d7326da2b428b837b39
elie <el415698@dal.ca>
2024-09-13 10:01:48 +0200

    fix login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AuthGuard } from './user/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent },
+  { path: '', component: HomepageComponent, canActivate: [AuthGuard] },
   {
     path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)
-  }
+  },
+
+
 ];
 
 @NgModule({
